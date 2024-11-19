f593bd7f9a3c2b75db86294a913bc2e549b06d1d
Lucy Heaney <2561323H@student.gla.ac.uk>
2024-11-19 11:44:00 +0000

    new files

---
+++ run_advdiff1D.m
@@ -0,0 +1,28 @@
+%***** RUN 1D ADVECTION DIFFUSION MODEL ***********************************
+
+% clear workspace
+clear all; close all; %clc;
+
+% set model parameters
+W  = 500;          % domain width [m]
+w  = 50;           % magma width [m]
+N  = 100;          % grid size
+dx = W/N;          % grid spacing
+
+T0   = 100;        % initial background temperature [C]
+dT   = 1000;       % initial temperature peak amplitude [C]
+wT   = 20;         % initial temperature peak width [m]
+k0   = 0e-6;       % heat diffusivity [m2/s]
+u0   = 1e-6;       % advection speed [m/s]
+BC   = 'periodic'; % boundary condition option flag ('insulating', 'periodic')
+ADVN = 'WENO5';    % advection scheme ('UPW1', 'CFD2', 'UPW3', 'WENO5')
+TINT = 'BE1';      % time integration scheme ('FE1','RK2','RK4')
+SCHEME = 'implicit';
+
+yr    = 3600*24*365;  % seconds per year [s]
+tend  = W/u0;         % stopping time [s]
+CFL   = 1/50;         % Time step limiter
+nop   = 50;          % output figure produced every 'nop' steps
+
+%*****  RUN MODEL
+run('./advdiff1D_solution.m');
