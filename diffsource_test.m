be9e0f08f68767b89bc6cbd661ab9d3fc4320f66
2561323H <2561323H@student.gla.ac.uk>
2024-12-04 19:49:20 +0000

    diffsource 2D 3.0

---
+++ diffsource_test.m
@@ -0,0 +1,91 @@
+%***** 2D diffusion model of heat transport *******************************
+
+%*****  Initialise Model Setup
+
+% create x-coordinate vectors
+xc = h/2:h:W-h/2;      % x-coordinate vector for cell centre positions [m]
+zc = h/2:h:D-h/2;      % z-coordinate vector for cell centre positions [m]
+xf = 0:h:W;            % x-coordinate vectore for cell face positions [m]
+zf = 0:h:D;            % z-coordinate vectore for cell face positions [m]
+[Xc,Zc] = meshgrid(xc,zc);  % create 2D coordinate arrays
+
+% set up index array for boundary conditions
+ix = [ 1,1:Nx,Nx ];  % closed/insulating sides
+iz = [ 1,1:Nz,Nz ];  % closed/insulating top, flux grad at bottom
+
+% set initial condition for temperature at cell centres
+T   = T0 + dTdz(2).*Zc;  % initialise T array on linear gradient
+
+
+% set up condition for air
+air = units == 9;
+
+%*****  Solve Model Equations
+
+t = 0;  % initial time [s]
+tau = 0;  % initial time step count
+dt = CFL * (h/2)^2/max(k0, [], 'all');
+
+while t <= tend
+
+    % increment time and step count
+    t = t+dt;
+    tau = tau+1;
+
+    % reset air section
+    T(air) = Tair;
+
+    % 4th-order Runge-Kutta time integration scheme
+            
+    dTdt1 = diffusion(T,dTdz,k0,h,ix,iz);
+    dTdt2 = diffusion(T+dTdt1/2*dt, dTdz, k0,h,ix,iz);
+    dTdt3 = diffusion(T+dTdt2/2*dt, dTdz,k0,h,ix,iz);
+    dTdt4 = diffusion(T+dTdt3  *dt, dTdz,k0,h,ix,iz);
+
+    T = T + (dTdt1 + 2*dTdt2 + 2*dTdt3 + dTdt4)/6 * dt + (Hr ./ rho ./ Cp);
+
+ 
+    % plot model progress every 'nop' time steps
+    if ~mod(tau,nop)
+        makefig(xc,zc,T);
+    end
+
+   
+end
+
+%*****  Utility Functions  ************************************************
+
+% Function to calculate diffusion rate
+function [dTdt] = diffusion(f, dTdz, k0, h, ix, iz)
+
+% average k0 values to get cell face values
+kx = k0(:, ix(1:end-1)) + k0(:, ix(2:end))/2;
+kz = k0(iz(1:end-1), :) + k0(iz(2:end), :)/2;
+
+% calculate heat flux by diffusion
+qx = - kx .* diff(f(:, ix), 1, 2)/h;
+qz = - kz .* diff(f(iz, :), 1, 1)/h;
+
+% set boundary conditions
+qz(end, :) = -kz(end, :) .* dTdz(2);
+
+% calculate flux balance for rate of change
+dTdt = -(diff(qx, 1, 2)/h + diff(qz, 1, 1)/h);
+
+end
+
+% Function to make output figure
+function makefig(x,z,T)
+
+clf; 
+
+% plot temperature
+imagesc(x,z,T); axis equal tight; colorbar; hold on
+ylabel('z [m]','FontSize',15)
+title('Temperature [C]','FontSize',17)
+[C,h] = contour(x,z,T, [150,150], 'r', 'Linewidth', 2);
+clabel(C,h,'Fontsize',12,'Color','r');
+
+drawnow;
+
+end
---
+++ run_diffsource_test.m
@@ -0,0 +1,52 @@
+%***** RUN 2D MODEL FROM IMAGE ***********************************
+
+% clear workspace
+clear all; close all; %clc;
+
+% load model setup from image, interpolate to target grid size
+W       = 16e3;     % domain width (must correspond to width of image) [m]
+Nx      = 200;      % target no. of columns in x-direction
+h       = W/Nx;     % grid spacing based on image width and target grid size
+n_units = 9;        % number of rock units contained in image
+
+% units = value of each pixel (colour)
+% D = original depth
+% Nz = target no. of rows in z-direction
+[units,D,Nz] = ModelFromImage('section.tiff',n_units,W,Nx);
+
+% material properties for each rock unit taken from provided excel file and
+% DOI: 10.1023/B:NARR.0000032647.41046.e7.
+
+matprop = [
+% unit  conductivity  density  heat capacity  heat production
+  1	    3.6788	    2697.6	    600 	    4.172           % Granite phase 1, alternative Cp 1172
+  2	    1	        2700	    770	        1               % Basement gneiss, alternative Cp 979
+  3	    3.2197	    2703.5	    600	        5.575           % Granite phase 2
+  4	    1	        1942.3	    740	        1               % Sand
+  5	    1	        2648	    740	        1               % Gravel
+  6	    0.924	    2081.7	    860	        1               % Clay, mudstone
+  7	    1	        1916	    910	        1               % Silt
+  8	    0.919	    1909.78	    740	        1               % Mud, silt, sand
+  9	    1e-6        1000	    1000	    0];             % air/water
+
+% get coefficient fields based on spatial distribution of rock units from image
+
+rho    = reshape(matprop(units,3),Nz,Nx); % density
+Cp     = reshape(matprop(units,4),Nz,Nx); % specific heat capacity
+kT     = reshape(matprop(units,2),Nz,Nx); % conductivity
+Hr     = reshape(matprop(units,5),Nz,Nx); % heat rate
+
+% calculate heat diffusivity [m2/s]
+k0 = kT ./ (rho .* Cp);
+
+% set model parameters
+dTdz = [0, 35/1000];  % set boundary condition
+T0  = 5;              % surface temperature degree C
+Tair = 5;             % air temperature degree C
+nop   = 100;          % output figure produced every 'nop' steps
+yr    = 3600*24*365;  % seconds per year [s]
+tend  = 1e7*yr;       % stopping time [s]
+CFL   = 1/5;         % Time step limiter
+
+%*****  RUN MODEL
+run('./diffsource_test.m');
