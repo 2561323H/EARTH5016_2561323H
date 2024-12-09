6d2f097297cf1d6a7144a3b3045ae76162a4934b
2561323H <2561323H@student.gla.ac.uk>
2024-12-09 17:22:34 +0000

    Add files via upload
    code 4.0: still have to do conv for t justa the same as x

--- diffsource_test.m
+++ diffsource_test.m
@@ -9,13 +9,15 @@ xf = 0:h:W;            % x-coordinate vectore for cell face positions [m]
 zf = 0:h:D;            % z-coordinate vectore for cell face positions [m]
 [Xc,Zc] = meshgrid(xc,zc);  % create 2D coordinate arrays
 
+
+
 % set up index array for boundary conditions
 ix = [ 1,1:Nx,Nx ];  % closed/insulating sides
 iz = [ 1,1:Nz,Nz ];  % closed/insulating top, flux grad at bottom
 
 % set initial condition for temperature at cell centres
 T   = T0 + dTdz(2).*Zc;  % initialise T array on linear gradient
-
+Ta = T;
 
 % set up condition for air
 air = units == 9;
@@ -35,23 +37,34 @@ while t <= tend
     % reset air section
     T(air) = Tair;
 
-    % 4th-order Runge-Kutta time integration scheme
-            
-    dTdt1 = diffusion(T,dTdz,k0,h,ix,iz);
-    dTdt2 = diffusion(T+dTdt1/2*dt, dTdz, k0,h,ix,iz);
-    dTdt3 = diffusion(T+dTdt2/2*dt, dTdz,k0,h,ix,iz);
-    dTdt4 = diffusion(T+dTdt3  *dt, dTdz,k0,h,ix,iz);
+            % 4th-order Runge-Kutta time integration scheme
+                    
+            dTdt1 = diffusion(T,dTdz,            k0,h,ix,iz);
+            dTdt2 = diffusion(T+dTdt1/2*dt, dTdz,k0,h,ix,iz);
+            dTdt3 = diffusion(T+dTdt2/2*dt, dTdz,k0,h,ix,iz);
+            dTdt4 = diffusion(T+dTdt3  *dt, dTdz,k0,h,ix,iz);
+        
+            T = T + (dTdt1 + 2*dTdt2 + 2*dTdt3 + dTdt4)/6 * dt + (Hr ./ rho ./ Cp);
 
-    T = T + (dTdt1 + 2*dTdt2 + 2*dTdt3 + dTdt4)/6 * dt + (Hr ./ rho ./ Cp);
-
- 
-    % plot model progress every 'nop' time steps
+    %plot model progress every 'nop' time steps
     if ~mod(tau,nop)
-        makefig(xc,zc,T);
+        makefig(xc,zc,T,t,yr);
     end
 
    
 end
+
+
+%*****  calculate numerical error norm
+Errx = norm(T - Ta,1)./norm(Ta,1);
+disp(' ');
+disp(['Numerical error = ',num2str(Errx)]);
+disp(' ');
+
+Errz = norm(T - Ta,1)./norm(Ta,1);
+disp(' ');
+disp(['Numerical error = ',num2str(Errz)]);
+disp(' ');
 
 %*****  Utility Functions  ************************************************
 
@@ -75,15 +88,16 @@ dTdt = -(diff(qx, 1, 2)/h + diff(qz, 1, 1)/h);
 end
 
 % Function to make output figure
-function makefig(x,z,T)
+function makefig(x,z,T,t,yr)
 
 clf; 
 
-% plot temperature
+%plot temperature
 imagesc(x,z,T); axis equal tight; colorbar; hold on
 ylabel('z [m]','FontSize',15)
-title('Temperature [C]','FontSize',17)
-[C,h] = contour(x,z,T, [150,150], 'r', 'Linewidth', 2);
+%title('Temperature [C]',,'FontSize',17);
+title(['Temperature; time = ',num2str(t/yr),' yr'],'FontSize',17)
+[C,h] = contour(x,z,T, [40,90, 140], 'r', 'Linewidth', 2);
 clabel(C,h,'Fontsize',12,'Color','r');
 
 drawnow;
--- run_diffsource_test.m
+++ run_diffsource_test.m
@@ -3,11 +3,19 @@
 % clear workspace
 clear all; close all; %clc;
 
+NN = [100,200,400];
+
+
+for nn = 1:3 
+
+
 % load model setup from image, interpolate to target grid size
 W       = 16e3;     % domain width (must correspond to width of image) [m]
-Nx      = 200;      % target no. of columns in x-direction
+Nx      = NN(nn);      % target no. of columns in x-direction
 h       = W/Nx;     % grid spacing based on image width and target grid size
 n_units = 9;        % number of rock units contained in image
+test = 'yes';
+
 
 % units = value of each pixel (colour)
 % D = original depth
@@ -18,35 +26,85 @@ n_units = 9;        % number of rock units contained in image
 % DOI: 10.1023/B:NARR.0000032647.41046.e7.
 
 matprop = [
-% unit  conductivity  density  heat capacity  heat production
-  1	    3.6788	    2697.6	    600 	    4.172           % Granite phase 1, alternative Cp 1172
-  2	    1	        2700	    770	        1               % Basement gneiss, alternative Cp 979
-  3	    3.2197	    2703.5	    600	        5.575           % Granite phase 2
-  4	    1	        1942.3	    740	        1               % Sand
-  5	    1	        2648	    740	        1               % Gravel
-  6	    0.924	    2081.7	    860	        1               % Clay, mudstone
-  7	    1	        1916	    910	        1               % Silt
-  8	    0.919	    1909.78	    740	        1               % Mud, silt, sand
-  9	    1e-6        1000	    1000	    0];             % air/water
+        % unit  conductivity  density  heat capacity  heat production
+          1	    3.6788	        2697.6	    600 	    4.172e-6           % Granite phase 1, alternative Cp 1172
+          2	    2.465           2700	    770	        2e-6               % Basement gneiss, alternative Cp 979
+          3	    3.2197	        2703.5	    600	        5.575e-6           % Granite phase 2
+          4	    0.77	        1942.3	    740	        0                  % Sand
+          5	    0.77	        2648	    740	        0                  % Gravel
+          6	    0.924	        2081.7	    860	        0                  % Clay, mudstone
+          7	    1.67	        1916	    910	        0                  % Silt
+          8	    0.919	        1909.78	    740	        0                  % Mud, silt, sand
+          9	    1e-6            1   	    1000	    0];                % air/water
+        
 
-% get coefficient fields based on spatial distribution of rock units from image
+switch test 
+    
+    case 'no' 
 
-rho    = reshape(matprop(units,3),Nz,Nx); % density
-Cp     = reshape(matprop(units,4),Nz,Nx); % specific heat capacity
-kT     = reshape(matprop(units,2),Nz,Nx); % conductivity
-Hr     = reshape(matprop(units,5),Nz,Nx); % heat rate
+        
+        % get coefficient fields based on spatial distribution of rock units from image
+        
+        rho    = reshape(matprop(units,3),Nz,Nx); % density
+        Cp     = reshape(matprop(units,4),Nz,Nx); % specific heat capacity
+        kT     = reshape(matprop(units,2),Nz,Nx); % conductivity
+        Hr     = reshape(matprop(units,5),Nz,Nx); % heat rate
+        
+        
+        % calculate heat diffusivity [m2/s]
+        k0 = kT*10^3 ./ rho ./ Cp;
 
-% calculate heat diffusivity [m2/s]
-k0 = kT ./ (rho .* Cp);
+    case 'yes'
+
+        rho    = 2400*ones(Nz,Nx); % density
+        Cp     = 1000*ones(Nz,Nx); % specific heat capacity
+        kT     = ones(Nz,Nx); % conductivity
+        Hr     = ones(Nz,Nx); % heat rate
+
+        % calculate heat diffusivity [m2/s]
+        k0 = kT*10^3 ./ rho ./ Cp;
+        
+end
 
 % set model parameters
 dTdz = [0, 35/1000];  % set boundary condition
-T0  = 5;              % surface temperature degree C
-Tair = 5;             % air temperature degree C
-nop   = 100;          % output figure produced every 'nop' steps
+T0  = 10;              % surface temperature degree C
+Tair = 10;             % air temperature degree C
+nop   = 5000;          % output figure produced every 'nop' steps
+wT   = 10;         % initial temperature peak width [m]
 yr    = 3600*24*365;  % seconds per year [s]
-tend  = 1e7*yr;       % stopping time [s]
+tend  = 1e6*yr;       % stopping time [s]
 CFL   = 1/5;         % Time step limiter
 
 %*****  RUN MODEL
 run('./diffsource_test.m');
+
+EX(nn) = Errx;
+EZ(nn) = Errz;
+DH(nn) = h;
+
+end
+
+%plot convergence figures for x and z
+
+figure(); 
+loglog(DH,EX,'ro','LineWidth',1.5,'MarkerSize',8); axis tight; box on; hold on
+loglog(DH,EX(1).*[1,1/2,1/4].^1,'k-','LineWidth',0.7)
+loglog(DH,EX(1).*[1,1/2,1/4].^2,'k-','LineWidth',0.9)
+loglog(DH,EX(1).*[1,1/2,1/4].^3,'k-','LineWidth',1.1)
+loglog(DH,EX(1).*[1,1/2,1/4].^4,'k-','LineWidth',1.3)
+loglog(DH,EX(1).*[1,1/2,1/4].^5,'k-','LineWidth',1.5)
+xlabel('Step size','FontSize',18)
+ylabel('Numerical error','FontSize',18)
+title('Numerical Convergence in Space','FontSize',20)
+
+figure(); 
+loglog(DH,EZ,'ro','LineWidth',1.5,'MarkerSize',8); axis tight; box on; hold on
+loglog(DH,EZ(1).*[1,1/2,1/4].^1,'k-','LineWidth',0.7)
+loglog(DH,EZ(1).*[1,1/2,1/4].^2,'k-','LineWidth',0.9)
+loglog(DH,EZ(1).*[1,1/2,1/4].^3,'k-','LineWidth',1.1)
+loglog(DH,EZ(1).*[1,1/2,1/4].^4,'k-','LineWidth',1.3)
+loglog(DH,EZ(1).*[1,1/2,1/4].^5,'k-','LineWidth',1.5)
+xlabel('Step size','FontSize',18)
+ylabel('Numerical error','FontSize',18)
+title('Numerical Convergence in Space','FontSize',20)
